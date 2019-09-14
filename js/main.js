d8e246a90e79b2e94c7375468fdb86ec0e947bb2
nnhuyen <huyen.nguyen@ttu.edu>
2019-09-13 19:59:41 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const svgWidth = 1000;
+const svgHeight = 1000;
+const wallWidth = 500;
+const wallHeight = 1000;
+const doorWidth = svgWidth - wallWidth;
+const doorHeight = 1000;
+const xPosition = 100;
+
+let svg = d3.select("body").append("svg")
+    .attr("width", svgWidth).attr("height", svgHeight);
+let doorGroup = svg.append("g");
+let wallGroup = svg.append("g").attr("transform", "translate(" + doorWidth + ",0)");
+
+wallGroup.append("rect")
+    .attr("width", wallWidth)
+    .attr("height", wallHeight)
+    .attr("fill", "#f1f1f1")
+
+let background = wallGroup.append('image')
+    .attr('xlink:href', 'images/background.jpg')
+    .attr('width', 200)
+    .attr('height', 400)
+.attr("transform", "translate(100, 300)");
+
