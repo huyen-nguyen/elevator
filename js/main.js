174ed6bb41c096395f6bba06c683514e8667f852
Huyen <huyen.nguyen@ttu.edu>
2019-09-17 02:10:28 -0500

    Update interface 1

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ const xPosition = 100;
 
 let svg = d3.select("body").append("svg")
     .attr("width", svgWidth).attr("height", svgHeight);
+
 let doorGroup = svg.append("g");
 let wallGroupSVG = svg.append("g").attr("transform", "translate(" + doorWidth + ",0)");
 let wallGroupDiv = d3.select("body").append("g");
@@ -22,7 +23,7 @@ wallGroupSVG.append("rect")
 
 let background = wallGroupSVG.append('image')
     .attr('xlink:href', 'images/background.jpg')
-    .attr('width', 200)
+    .attr('width', 400)
     .attr('height', 400)
 .attr("transform", "translate(100, 300)");
 
